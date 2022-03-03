43ea67ec4195195020eb6994a5cc150696bc9c77
pebend <67839328+pebend@users.noreply.github.com>
2022-03-03 12:22:50 -0500

    Add files via upload

---
+++ Lab2.R
@@ -0,0 +1,178 @@
+pairs(~ Fertility + Education + Catholic, data = swiss, subset = Education < 20, main = "Swiss data, Education < 20")
+require(party)
+swiss_ctree <- ctree(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+plot(swiss_ctree)
+
+require(kknn)
+data(iris)
+m <- dim(iris)[1]
+val <- sample(1:m, size = round(m/3), replace = FALSE, prob = rep(1/m, m)) 
+iris.learn <- iris[-val,]
+iris.valid <- iris[val,]
+iris.kknn <- kknn(Species~., iris.learn, iris.valid, distance = 1,kernel = "triangular")
+summary(iris.kknn)
+fit <- fitted(iris.kknn)
+table(iris.valid$Species, fit)
+pcol <- as.character(as.numeric(iris.valid$Species))
+
+require(kknn)
+data(ionosphere)
+ionosphere.learn <- ionosphere[1:200,]
+ionosphere.valid <- ionosphere[-c(1:200),]
+fit.kknn <- kknn(class ~ ., ionosphere.learn, ionosphere.valid)
+table(ionosphere.valid$class, fit.kknn$fit)
+(fit.train1 <- train.kknn(class ~ ., ionosphere.learn, kmax = 15, kernel = c("triangular", "rectangular", "epanechnikov", "optimal"), distance = 1))
+table(predict(fit.train1, ionosphere.valid), ionosphere.valid$class)
+(fit.train2 <- train.kknn(class ~ ., ionosphere.learn, kmax = 15, 
+	kernel = c("triangular", "rectangular", "epanechnikov", "optimal"), distance = 2))
+table(predict(fit.train2, ionosphere.valid), ionosphere.valid$class)
+
+data(swiss)
+pairs(~ Fertility + Education + Catholic, data = swiss, subset = Education < 20, main = "Swiss data, Education < 20")
+
+library(gdata) 
+#faster xls reader but requires perl!
+bronx1<-read.xls(file.choose(),pattern="BOROUGH",stringsAsFactors=FALSE,sheet=1,perl="<SOMEWHERE>/perl/bin/perl.exe") 
+bronx1<-bronx1[which(bronx1$GROSS.SQUARE.FEET!="0" & bronx1$LAND.SQUARE.FEET!="0" & bronx1$SALE.PRICE!="$0"),]
+
+View(bronx1)
+#
+attach(bronx1) # If you choose to attach, leave out the "data=." in lm regression
+SALE.PRICE<-sub("\\$","",SALE.PRICE) 
+SALE.PRICE<-as.numeric(gsub(",","", SALE.PRICE)) 
+GROSS.SQUARE.FEET<-as.numeric(gsub(",","", GROSS.SQUARE.FEET)) 
+LAND.SQUARE.FEET<-as.numeric(gsub(",","", LAND.SQUARE.FEET)) 
+plot(log(GROSS.SQUARE.FEET), log(SALE.PRICE)) 
+m1<-lm(log(SALE.PRICE)~log(GROSS.SQUARE.FEET))
+summary(m1)
+abline(m1,col="red",lwd=2)
+plot(resid(m1))
+
+# Model 2
+
+m2<-lm(log(bronx1$SALE.PRICE)~log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD))
+summary(m2)
+plot(resid(m2))
+m2a<-lm(log(bronx1$SALE.PRICE)~0+log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD))
+summary(m2a)
+plot(resid(m2a))
+
+# Model 3
+m3<-lm(log(bronx1$SALE.PRICE)~0+log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD)+factor(bronx1$BUILDING.CLASS.CATEGORY))
+summary(m3)
+plot(resid(m3))
+
+# Model 4
+m4<-lm(log(bronx1$SALE.PRICE)~0+log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD)*factor(bronx1$BUILDING.CLASS.CATEGORY))
+summary(m4)
+plot(resid(m4))
+#
+
+bronx1$SALE.PRICE<-sub("\\$","",bronx1$SALE.PRICE) 
+bronx1$SALE.PRICE<-as.numeric(gsub(",","", bronx1$SALE.PRICE)) 
+bronx1$GROSS.SQUARE.FEET<-as.numeric(gsub(",","", bronx1$GROSS.SQUARE.FEET)) 
+bronx1$LAND.SQUARE.FEET<-as.numeric(gsub(",","", bronx1$LAND.SQUARE.FEET)) 
+bronx1$SALE.DATE<- as.Date(gsub("[^]:digit:]]","",bronx1$SALE.DATE)) 
+bronx1$YEAR.BUILT<- as.numeric(gsub("[^]:digit:]]","",bronx1$YEAR.BUILT)) 
+bronx1$ZIP.CODE<- as.character(gsub("[^]:digit:]]","",bronx1$ZIP.CODE)) 
+
+minprice<-10000
+bronx1<-bronx1[which(bronx1$SALE.PRICE>=minprice),]
+nval<-dim(bronx1)[1]
+
+bronx1$ADDRESSONLY<- gsub("[,][[:print:]]*","",gsub("[ ]+","",trim(bronx1$ADDRESS))) bronxadd<-unique(data.frame(bronx1$ADDRESSONLY, bronx1$ZIP.CODE,stringsAsFactors=FALSE)) names(bronxadd)<-c("ADDRESSONLY","ZIP.CODE") bronxadd<-bronxadd[order(bronxadd$ADDRESSONLY),] duplicates<-duplicated(bronx1$ADDRESSONLY)
+
+for(i in 1:2345) {
+  if(duplicates[i]==FALSE) dupadd<-bronxadd[bronxadd$duplicates,1]
+}#what are we doing with dupadd?
+
+nsample=450
+
+addsample<-bronxadd[sample.int(dim(bronxadd),size=nsample),]#I use nval here 
+# may need to install this package
+library(ggmap)
+addrlist<-paste(addsample$ADDRESSONLY, "NY", addsample$ZIP.CODE, "US", sep=" ") 
+querylist<-geocode(addrlist) #This is cool. Take a break.
+
+matched<-(querylist$lat!=0 &&querylist$lon!=0) addsample<-cbind(addsample,querylist$lat,querylist$lon) 
+names(addsample)<-c("ADDRESSONLY","ZIPCODE","Latitude","Longitude")# correct the column na adduse<-merge(bronx1,addsample)
+
+adduse<-adduse[!is.na(adduse$Latitude),]
+mapcoord<-adduse[,c(2,3,24,25)]
+
+table(mapcoord$NEIGHBORHOOD)
+
+mapcoord$NEIGHBORHOOD <- as.factor(mapcoord$NEIGHBORHOOD)
+map <- get_map(location = 'Bronx', zoom = 12)#Zoom 11 or 12
+ggmap(map) + geom_point(aes(x = mapcoord$Longitude, y = mapcoord$Latitude, size =1, color=mapcoord$NEIGHBORHOOD), data = mapcoord) +theme(legend.position = "none") 
+
+#It would be perfect if I can decrease the size of points 
+
+mapmeans<-cbind(adduse,as.numeric(mapcoord$NEIGHBORHOOD))
+colnames(mapmeans)[26] <- "NEIGHBORHOOD" #This is the right way of renaming.
+
+keeps <- c("ZIP.CODE","NEIGHBORHOOD","TOTAL.UNITS","LAND.SQUARE.FEET","GROSS.SQUARE.FEET","SALE.PRICE","Latitude","Longitude") 
+mapmeans<-mapmeans[keeps]#Dropping others
+mapmeans$NEIGHBORHOOD<-as.numeric(mapcoord$NEIGHBORHOOD) 
+
+for(i in 1:8){
+  mapmeans[,i]=as.numeric(mapmeans[,i]) 
+}#Now done for conversion to numeric
+
+#Classification
+mapcoord$class<as.numeric(mapcoord$NEIGHBORHOOD)
+nclass<-dim(mapcoord)[1]
+split<-0.8
+trainid<-sample.int(nclass,floor(split*nclass))
+testid<-(1:nclass)[-trainid]
+kmax<-10
+knnpred<-matrix(NA,ncol=kmax,nrow=length(testid))
+knntesterr<-rep(NA,times=kmax)
+for (i in 1:kmax){		# loop over k
+  knnpred[,i]<-knn(mapcoord[trainid,3:4],mapcoord[testid,3:4],cl=mapcoord[trainid,2],k=i)
+  knntesterr[i]<-sum(knnpred[,i]!=mapcoord[testid,2])/length(testid)
+} 
+knntesterr
+
+#Clustering
+mapobj<-kmeans(mapmeans,5, iter.max=10, nstart=5, algorithm = c("Hartigan-Wong", "Lloyd", "Forgy", "MacQueen"))
+fitted(mapobj,method=c("centers","classes"))
+mapobj$centers
+#
+library(cluster)
+clusplot(mapmeans, mapobj$cluster, color=TRUE, shade=TRUE, labels=2, lines=0) 
+#
+library(fpc)#May need to install.packages("fpc")
+plotcluster(mapmeans, mapobj$cluster)
+#
+mapmeans1<-mapmeans[,-c(1,3,4)]
+mapobjnew<-kmeans(mapmeans1,5, iter.max=10, nstart=5, algorithm = c("Hartigan-Wong", "Lloyd", "Forgy", "MacQueen"))
+fitted(mapobjnew,method=c("centers","classes"))
+clusplot(mapmeans1, mapobjnew$cluster, color=TRUE, shade=TRUE, labels=2, lines=0) 
+plotcluster(mapmeans1, mapobjnew$cluster)
+ggmap(map) + geom_point(aes(x = mapcoord$Longitude, y = mapcoord$Latitude, size =1, color=mapobjnew$cluster), data = mapcoord)#How to change colors?
+
+require(rpart)
+Swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+plot(swiss_rpart) # try some different plot options
+text(swiss_rpart) # try some different text options
+
+require(party)
+
+treeSwiss<-ctree(Species ~ ., data=iris)
+plot(treeSwiss)
+
+cforest(Species ~ ., data=iris, controls=cforest_control(mtry=2, mincriterion=0))
+
+treeFert<-ctree(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+
+cforest(Fertility ~ Agriculture + Education + Catholic, data = swiss, controls=cforest_control(mtry=2, mincriterion=0))
+# look at help info, vary parameters.
+
+library(tree)
+tr <- tree(Species ~ ., data=iris)
+tr
+tr$frame
+plot(tr)
+text(tr)
+#find "prettier" ways to plot the tree
